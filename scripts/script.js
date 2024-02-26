dd7efacfe60b634a0ec80d9f448f6806bab2f87b
Tw3kda <santiago2006nc@gmail.com>
2024-02-26 01:47:54 -0500

    finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
-console.log("JavaScript working!");
+
+const input = document.getElementById("field");
+const list = document.getElementById("tasks");
+
+function addTask(){
+    if(input.value === ''){
+        alert("You have to write something");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    input.value="";
+}
+
+list.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+})
